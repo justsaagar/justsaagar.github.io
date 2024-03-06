56d2a54099d119d28585f549767e343ffa787d46
Sagar R Anghan <anghansagar25@gmail.com>
2024-03-06 18:24:57 +0530

    Add: Service commission

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "103468b890a3cf396d875205aca3e39a",
-"index.html": "bfadd8e338c203d627fea529e9558640",
-"/": "bfadd8e338c203d627fea529e9558640",
+"index.html": "afad2b3e5cbe493e0ca83c0f42df6e53",
+"/": "afad2b3e5cbe493e0ca83c0f42df6e53",
 "firebase-messaging-sw.js": "04b461781e9ad5034508857054d16f85",
-"main.dart.js": "74662e2e5e25ca9825f849c4ff31069a",
+"main.dart.js": "842a831189f87254fa43c2ee4d5cf520",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "2ad632a7e6bcceab993ebb0dd4a8ad0b",
 "icons/Icon-192.png": "75c731a289a633c35444b7714c73bbef",
