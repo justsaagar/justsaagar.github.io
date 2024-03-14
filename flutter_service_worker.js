54d05b992a5a43efc7a9c77cd608060b002a7ffb
Sagar R Anghan <anghansagar25@gmail.com>
2024-03-15 03:13:45 +0530

    UPdate: Product screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "103468b890a3cf396d875205aca3e39a",
-"index.html": "00f5e61117c780d8297ebb9ca42f690b",
-"/": "00f5e61117c780d8297ebb9ca42f690b",
+"index.html": "8e0d01b6189c02c88c23ea9670e6a658",
+"/": "8e0d01b6189c02c88c23ea9670e6a658",
 "firebase-messaging-sw.js": "04b461781e9ad5034508857054d16f85",
-"main.dart.js": "5b8216f844cf5d9442a8233bd15f5bd7",
+"main.dart.js": "3a238e088c0e689ba79ba9738d1fb9c9",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "2ad632a7e6bcceab993ebb0dd4a8ad0b",
 "icons/Icon-192.png": "75c731a289a633c35444b7714c73bbef",
